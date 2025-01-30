68cd5deeb5604e77b0efe46a85671f1254b8f2fa
Nicole <nicolecdverial@gmail.com>
2025-01-30 13:30:40 +0800

    [__init__] Initialized constructor

--- Fraction.py
+++ Fraction.py
@@ -2,7 +2,11 @@ class Fraction(object):
 
     def __init__(self, numerator=0, denominator=1):
         #TODO
-        pass
+        numerator = numerator
+        if denominator != 0:
+            raise ZeroDivisionError
+        else:
+            denominator = denominator
 
     def gcd(a, b):
         #TODO
